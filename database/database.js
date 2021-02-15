6b00fbdd5040e3d33039ec3f9e25eab68ce67357
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2021-02-15 10:46:37 -0300

    change db connection file

--- database/database.js
+++ database/database.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize('db_krekBot', 'root', '25062001', {
-    host: 'localhost', // host - onde esta rodando o db
+const userHeroku = "bf1fb496c0d7ca";
+const passwordHeroku = "2e305c55";
+const hostHeroku = "us-cdbr-east-03.cleardb.com";
+const herokuDatabase = "heroku_96be19fccdb63e1";
+const connection = new Sequelize(herokuDatabase, userHeroku, passwordHeroku, { //db_krekBot, root, 25062001
+    host: hostHeroku, // host - onde esta rodando o db -> localhost
     dialect: 'mysql', // qual banco de dados estamos utilizando
     logging: false
 });
